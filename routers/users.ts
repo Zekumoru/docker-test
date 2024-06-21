90147c5d696b633d9019a5318cd1a4565382067a
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-06-22 01:03:44 +0200

    Add users

--- routers/users.ts
+++ routers/users.ts
@@ -1,13 +1,23 @@
 import express from 'express';
 import createJsonResponse from '../utilities/createJsonResponse';
 import User from '../types/User';
+import { v4 as uuid4 } from 'uuid';
 
 const usersRouter = express.Router();
 
 usersRouter.get('/', (req, res) => {
   res.json(
     createJsonResponse<{ users: User[] }>({
-      users: [],
+      users: [
+        {
+          id: uuid4(),
+          name: 'Zekumoru',
+        },
+        {
+          id: uuid4(),
+          name: 'Yuzuki',
+        },
+      ].sort((a, b) => a.name.localeCompare(b.name)),
     })
   );
 });
